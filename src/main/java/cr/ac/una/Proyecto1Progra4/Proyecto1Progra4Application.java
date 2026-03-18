61bb44e2c8ff96e23bf70d8df1fdcb8287c58af9
AndresPzk <andresbaltodano11uni@gmail.com>
2026-03-17 19:42:55 -0600

    Se agrega el proyecto

---
+++ src/main/java/cr/ac/una/Proyecto1Progra4/Proyecto1Progra4Application.java
@@ -0,0 +1,13 @@
+package cr.ac.una.Proyecto1Progra4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Proyecto1Progra4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Proyecto1Progra4Application.class, args);
+	}
+
+}
---
+++ src/test/java/cr/ac/una/Proyecto1Progra4/Proyecto1Progra4ApplicationTests.java
@@ -0,0 +1,13 @@
+package cr.ac.una.Proyecto1Progra4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Proyecto1Progra4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
